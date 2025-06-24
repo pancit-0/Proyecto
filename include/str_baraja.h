b924ddbde8be20768d030e2f170463c7a0d6ee6a
pancit-0 <pancitochaconb@gmail.com>
2025-06-23 19:35:31 -0600

    Estructura en estado base

---
+++ include/str_baraja.h
@@ -0,0 +1,16 @@
+#ifndef BARAJA_H
+#define BARAJA_H
+
+#define NUM_CARTAS 52
+
+typedef struct {
+        Carta cartas[NUM_CARTAS];
+        int cantidad;
+} Baraja;
+
+void inicializar_baraja(Baraja *baraja);
+void mezclar_baraja(Baraja *baraja);
+Carta repartir(Baraja *baraja);
+
+#endif
+
